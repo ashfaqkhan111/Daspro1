d73b55e929d74e5c70a5dbc978a9ab8986f3da6e
Ashfaq Khan <ahfaqkhan97@gmail.com>
2025-09-30 09:15:18 +0700

    Update practice.java

--- src/practice/practice.java
+++ src/practice/practice.java
@@ -1,9 +1,31 @@
-package practice;
+package prtc;
+import java.util.Scanner;
 
-public class practice {
-
-	public practice() {
-		// TODO Auto-generated constructor stub
-	}
-
+public class prtc{
+	
+	public static void main (String[]args) {
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		double driverFee = 200000;
+		double fuelFee = 10000;
+		double carRantelPrice = 300000;
+		
+		
+		System.out.println("***Enter The Values***");
+	System.out.print("Enter rental period = ");
+	int rentaldays = input.nextInt();
+	
+	System.out.print("Enter distance travaled = ");
+	double distance = input.nextDouble();
+	
+double totalcost =  (carRantelPrice * rentaldays) + (driverFee * rentaldays) + (fuelFee * distance);
+	System.out.println("***Results***");
+	System.out.println("the tolal cost is = " +totalcost);
+	
+	input.close();
+	
+	}	
+	
 }
